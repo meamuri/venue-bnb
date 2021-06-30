a65b41d2fde5af689f28976ac108b9022a730d55
Roman Dronov <meammf@yandex.ru>
2021-06-30 14:30:43 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dr.meamuri/venue-bnb "0.1.0"
+  :description "Venue-bnb algorithms."
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+
+  :profiles
+  {:dev
+   {:source-paths ["env/dev"]}})
