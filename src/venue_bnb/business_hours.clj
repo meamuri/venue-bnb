7e8788b84923a124b2a6d58a565babbebb1e6240
Roman Dronov <meammf@yandex.ru>
2021-07-01 23:16:25 +0300

    Design algorithm.

--- src/venue_bnb/business_hours.clj
+++ src/venue_bnb/business_hours.clj
@@ -10,30 +10,20 @@
 
 (defn- date->local-date-time
   ""
-  [date]
+  [^Date date]
   (LocalDateTime/ofInstant (.toInstant date) ZoneOffset/UTC))
 
 (defn- days
   ""
-  [^java.util.Date start]
-  (let [start* (date->local-date-time start)]
-    (iterate #(.plusDays % 1) start*)))
+  [^LocalDateTime start]
+  (iterate #(.plusDays % 1) start))
 
 (defn- days-range
   ""
-  ([^Date start]
-   (days-range start (Date/from (Instant/now))))
-  ([^Date start ^Date end]
+  ([^LocalDateTime start]
+   (days-range start (LocalDateTime/now ZoneOffset/UTC)))
+  ([^LocalDateTime start ^LocalDateTime end]
    (take-while #(.isBefore % end) (days start))))
-
-(defn- scedule-entity->date
-  ""
-  [schedule-entity])
-
-(defn- schedule->day-of-weeks-to-hours
-  ""
-  [acc curr]
-  (let [day-of-week (DayOfWeek/from 3)]))
 
 (defn- flattent-schedule
   ""
@@ -47,11 +37,34 @@
               [(DayOfWeek/of k) v]))
        (into {})))
 
+(defn- processor
+  ""
+  [schedule]
+  (fn [{:keys [day-of-week from to]}]
+    (let [hours (get schedule day-of-week)]
+      (->> hours
+           (filter #(and (> % from) (< % to)))  ;; TODO: fix it, it is'not identity!
+           count))))
+
+(defn- day->internal-representation
+  ""
+  [^LocalDateTime start ^LocalDateTime to]
+  (fn [^LocalDateTime day]
+    (let [date (.toLocalDate day)]
+      {:day-of-week (.getDayOfWeek day)
+       :from (if (= (.toLocalDate start) date) (.getHour start) 0)
+       :to (if (= (.toLocalDate to) date) (.getHour to) 24)})))
+
 (defn calculate-business-hours
   ""
-  [schedule start-date end-date]
-  (let [days (days-range start-date end-date)
-        day-of-week-to-hours (reduce schedule->day-of-weeks-to-hours {} schedule)]))
+  [schedule ^Date start-date ^Date end-date]
+  (let [start (date->local-date-time start-date)
+        to (date->local-date-time end-date)
+        days (days-range start to)
+        prepared-days (map (day->internal-representation start to) days)
+        schedule' (flattent-schedule schedule)
+        hours-per-day (map (processor schedule') prepared-days)]
+    (reduce + 0 hours-per-day)))
 
 (defn calculate-date
   ""
