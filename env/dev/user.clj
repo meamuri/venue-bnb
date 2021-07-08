17c1b075ab67fe28895306932cf6582aa59a0e05
Roman Dronov <meammf@yandex.ru>
2021-07-08 19:56:44 +0300

    Implement scoring module.

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,1 +1,8 @@
-(ns user)
+(ns user
+  (:require [venue-bnb.scoring :as scoring]
+            [clojure.edn :as edn]))
+
+(comment
+  (-> (slurp "./sample-data.edn")
+      edn/read-string
+      scoring/score))
--- src/venue_bnb/scoring.clj
+++ src/venue_bnb/scoring.clj
@@ -1,4 +1,29 @@
-(ns venue-bnb.scoring)
+(ns venue-bnb.scoring
+  "")
+
+(defn- rating-weight [rating reviews]
+  (+ (* rating 1.1) (* reviews 1.8)))
+
+(defn- booking-weight [booking-rqeuest reservations]
+  (+ (* booking-rqeuest 1.1) (* reservations 1.0)))
+
+(defn- compute-score
+  [{:keys [booking-requests reservations reviews rating]}]
+  (let [rating-weight (rating-weight (or rating 0) (or reviews 0))
+        booking-weight (booking-weight (or booking-requests 0) (or reservations 0))]
+    (+ rating-weight booking-weight)))
+
+(defn- normalize
+  ""
+  [e]
+  (let [score (compute-score e)]
+    (assoc e :score score)))
 
 (defn score
-  [items])
+  [items]
+  (let [sort (partial sort-by :score)]
+    (->> items
+         (map normalize)
+         sort
+         (map #(dissoc % :score))
+         reverse)))
