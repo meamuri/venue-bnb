651932d99f562e3ff6ebd64031c689a67a7dceed
Roman Dronov <meammf@yandex.ru>
2021-07-09 22:41:13 +0300

    Design additional algorithm.

--- src/venue_bnb/scoring_algo.clj
+++ src/venue_bnb/scoring_algo.clj
@@ -1,8 +1,48 @@
 (ns venue-bnb.scoring-algo
   (:require [venue-bnb.scoring.validation :refer [validate]]))
 
-(defn- compare-venues [a b]
-  -1)
+(defn- reviews-compare
+  [lft rgt]
+  (let [{l-rating :rating
+         l-reviews :reviews} lft
+        {r-rating :rating
+         r-reviews :reviews} rgt
+        ratio (max (/ l-reviews r-reviews) (/ r-reviews l-reviews))]
+    (if (> ratio 4)
+      ;; if difference between reviews is significant, just return reviews winner
+      ;; in this case rating difference doesn't matter
+      (comp l-reviews r-reviews)
+      ;; here reviews ration check
+      (if (and (not= l-rating r-rating)
+               (> (Math/abs (- l-rating r-rating)) ratio))
+        ;; if difference betweeb rating more than
+        (comp l-rating r-rating)
+        ;; if rating is comparable let's use anouther metrics: we can't make decision
+        nil))))
+
+(defn- booking-compare
+  [lft rgt]
+  (let [{l-booking-requests :booking-requests
+         l-reservations :reservations} lft
+        {r-booking-requests :booking-requests
+         r-reservations :reservations} rgt]
+    (if (not= l-reservations r-reservations)
+      (comp l-reservations r-reservations)
+      (if (not= l-booking-requests r-booking-requests)
+        (comp l-booking-requests r-booking-requests)
+        nil))))
+
+(defn- compare-venues
+  [lft rgt]
+  ;; must significant criteria: by rating.
+  (if-let [reviews-ration (reviews-compare lft rgt)]
+    reviews-ration
+    ;; if rating almost similar, go to check by booking ration
+    (if-let [booking-ratio (booking-compare lft rgt)]
+      booking-ratio
+      ;; if entities almost similar by previous criterias
+      ;; just sort by name ))
+      (comp (:venue lft) (:venue rgt)))))
 
 (defn ^{:added "0.1.0"} score
   "Scores and sorts 'popular' venues.
